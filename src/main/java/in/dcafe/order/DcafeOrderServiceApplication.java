7140cc791bfbe6080477942b21791caaeb5590f5
Kalyan1718 <durgakalyan018@gmail.com>
2024-08-14 13:55:43 +0530

    First Commit

---
+++ src/main/java/in/dcafe/order/DcafeOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package in.dcafe.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcafeOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcafeOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dcafe/order/DcafeOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dcafe.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcafeOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
